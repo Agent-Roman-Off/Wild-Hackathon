1460bfa75a075806a1dd405c7dfd219033c76424
fortanza <undertaker040880@gmail.com>
2021-11-26 10:59:53 +0100

    Fix problem

---
+++ src/Components/Decade00s/00s.js
@@ -0,0 +1,74 @@
+const decade00 = [
+  {
+    id: 40,
+    name: 'Can We Fix It?',
+    artist: 'Bob the Builder',
+    release: 'December 4, 2000',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e029d4daee222b6acf39bef3187',
+  },
+  {
+    id: 41,
+    name: "It Wasn't Me",
+    artist: 'Shaggy (Ft. Rikrok)',
+    release: 'November 7, 2000',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e029449f9491c364d28a9668e17',
+  },
+  {
+    id: 42,
+    name: 'Anything Is Possible/Evergreen',
+    artist: 'Will Young',
+    release: 'February 25, 2002',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e02904e0b062e23f028f4348916',
+  },
+  {
+    id: 43,
+    name: 'Where Is the Love?',
+    artist: 'Black Eyed Peas',
+    release: 'May 12, 2003',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e029eb5f4d79e7027cbb6d5881d',
+  },
+  {
+    id: 44,
+    name: "Do They Know It's Christmas?",
+    artist: 'Band Aid 20',
+    release: 'November 29, 2004',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e02fbd6fd461285a2fe0b3e0a0a',
+  },
+  {
+    id: 45,
+    name: '(Is This the Way to) Amarillo',
+    artist: 'Tony Christie (Ft. Peter Kay)',
+    release: 'March 14, 2005',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e0267e7a8a15f3c2618fbc4804b',
+  },
+  {
+    id: 46,
+    name: 'Crazy',
+    artist: 'Gnarls Barkley',
+    release: 'March 13, 2006',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e022cb3f40f7d4114236061f77f',
+  },
+  {
+    id: 47,
+    name: 'Bleeding Love',
+    artist: 'Leona Lewis',
+    release: 'October 19, 2007',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e0234fd9eb8cd48e518598aec55',
+  },
+  {
+    id: 48,
+    name: 'Hallelujah',
+    artist: 'Alexandra Burke',
+    release: 'December 17, 2008',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e02ef5b8a9e457c26494a2ae724',
+  },
+  {
+    id: 49,
+    name: 'Poker Face',
+    artist: 'Lady Gaga',
+    release: 'September 23, 2008',
+    artwork: 'https://i.scdn.co/image/ab67616d00001e02e691217483df8798445c82e2',
+  },
+];
+
+export default decade00;
--- src/Components/DecadeDetails/DecadeDetails.jsx
+++ src/Components/DecadeDetails/DecadeDetails.jsx
@@ -3,17 +3,17 @@ import './DecadeDetails.scss';
 const DecadeDetails = () => {
   return (
     <div className="Parent">
-      <div className="div1 red" />
-      <div className="div2 red"> </div>
-      <div className="div3 red"> </div>
-      <div className="div4 red"> </div>
-      <div className="div5 red"> </div>
-      <div className="div6 red"> </div>
-      <div className="div7 red"> </div>
-      <div className="div8 red"> </div>
-      <div className="div9 red"> </div>
-      <div className="div10 red"> </div>
-      <div className="div11 red"> </div>
+      <div className="div1 glass" />
+      <div className="div2 glass"> xxxx[0]</div>
+      <div className="div3 glass"> </div>
+      <div className="div4 glass"> </div>
+      <div className="div5 glass"> </div>
+      <div className="div6 glass"> </div>
+      <div className="div7 glass"> </div>
+      <div className="div8 glass"> </div>
+      <div className="div9 glass"> </div>
+      <div className="div10 glass"> </div>
+      <div className="div11 glass"> </div>
     </div>
   );
 };
